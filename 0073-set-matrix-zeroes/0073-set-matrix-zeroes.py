764647d6784b03244184a58b09f67dd375043f54
Nadia Shahreen <nadiashahreen12345@gmail.com>
2024-12-02 17:07:28 +0530

    Time: 11 ms (8.34%), Space: 18 MB (6.41%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,19 +1,24 @@
+def markzero(matrix,row,column,j):#column is fixed just iterate through rows
+    for i in range(row):
+        if matrix[i][j]!=0:
+            matrix[i][j]="x"
+def markzeroc(matrix,row,column,i):#row is fixed just iterate through rows
+    for j in range(column):
+        if matrix[i][j]!=0:
+            matrix[i][j]="x"
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        row=[]
-        column=[]
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
-                    row.append(i)
-                    column.append(j)
+                    markzero(matrix,len(matrix),len(matrix[0]),j)
+                    markzeroc(matrix,len(matrix),len(matrix[0]),i)
+       
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                if i in row or j in column:
-                    if matrix[i][j]!=0:
-                        matrix[i][j]=0
+                if matrix[i][j]=="x":
+                    matrix[i][j] = 0
         print(matrix)
-        
