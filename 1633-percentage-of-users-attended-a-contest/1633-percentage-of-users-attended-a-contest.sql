16fdc8de46e73d41d2d624ae68d9df3aa479f262
Nadia Shahreen <nadiashahreen12345@gmail.com>
2026-06-22 16:00:11 +0530

    Time: 1007 ms (50.3%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,8 @@
+select r.contest_id, round(
+    count(r.user_id) * 100/ (select count(user_id) from users),2
+    ) as percentage 
+from users u
+inner join register r
+on u.user_id = r.user_id
+group by r.contest_id
+order by percentage desc, contest_id;
