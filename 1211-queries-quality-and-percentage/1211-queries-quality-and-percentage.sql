e1b326976de1d82cb9aaa85f1547a00317c3c40c
Nadia Shahreen <nadiashahreen12345@gmail.com>
2026-06-24 15:31:30 +0530

    Time: 376 ms (55.48%), Space: 0B (100%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,6 @@
+select query_name,round(avg(rating/position),2) as quality,ROUND(
+        SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100 / COUNT(*),
+        2
+    ) AS poor_query_percentage
+from queries
+group by query_name;
